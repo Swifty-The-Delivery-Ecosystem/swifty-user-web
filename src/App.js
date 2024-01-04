92540289170d4e96af0751eba9aaa6fd67227f9b
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-01-04 14:36:57 +0530

    feat: Added login and signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import RestaurantScreen from './pages/restaurant';
-import res1 from './sampleData/restaurantData';
-
-
+import "./App.css";
+import Login from "./pages/login_page";
+import RestaurantScreen from "./pages/restaurant";
+import SignUp from "./pages/signup_page";
+import res1 from "./sampleData/restaurantData";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App text-3xl font-bold underline">
-    Swifty
-    <RestaurantScreen restaurant={res1} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RestaurantScreen restaurant={res1} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
